16ac30042edcaabc2da19568450324b57e79a201
Yuvraj Sharma <65121609+Sharma-Yuvraj@users.noreply.github.com>
2021-12-21 20:33:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 8000;
 const homeFile = fs.readFileSync("home.html", "UTF-8");
 const replaceVal = (tempVal, orgVal) => {
     let temperature = tempVal.replace("{%tempVal%}", Math.floor(orgVal.main.temp - 273.15));
